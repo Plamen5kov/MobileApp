9203c9153a52da8579581b1e702a011d9ef35796
Пламен Петков <plamen5kov@abv.bg>
2014-09-28 05:11:55 -0500

    extracted methods into different files

---
+++ app/scripts/add-picture.js
@@ -0,0 +1,100 @@
+(function () {
+    var images = everlive.data("PictureInfo");
+
+    var loadPhoto = function () {
+        images.get()
+            .then(function (data) {
+                    console.log(data);
+                    var files = [];
+                    data.result.forEach(function (image) {
+                        files.push(image.Img.Uri);
+                    });
+
+                    $('#images').kendoMobileListView({
+                        dataSource: files,
+                        template: '<img src="#: data #">'
+                    });
+                },
+                function (error) {
+                    console.log(error);
+                })
+    };
+
+    //if there are any photos already in the db ... show them immediatly
+    loadPhoto();
+
+    var geolocationError = function (data) {
+        console.log('---some err---');
+        console.log(data);
+    };
+
+    var geolocationOptions = {
+        maximumAge: 3000,
+        timeout: 5000,
+        enableHighAccuracy: true
+    }
+
+    function createEverliveObject(data) {
+        console.log('FINAL DATA TO SAVE IN EVERLIVE...');
+        console.log(data.img);
+        everlive.Files.create({ //if you took the pic...create a file in everlive
+            Filename: Math.random().toString(36).substring(2, 15) + ".jpg", //give it random name
+            ContentType: "image/jpg", // config
+            base64: data //more config
+        })
+            .then(loadPhoto);
+    }
+     window.listView = kendo.observable({
+        addImage: function () {
+            var location = {};
+
+            var picSuccess = function (data) {
+
+                window.everlive.Files.create({ //if you took the pic...create a file in everlive
+                        Filename: Math.random().toString(36).substring(2, 15) + ".jpg", //give it random name
+                        ContentType: "image/jpg", // config
+                        base64: data //more config
+                    },
+                    function (picData) {
+                        window.everlive.data('PictureInfo').create({
+                                'Img': picData.result,
+                                'Location': location
+                            },
+                            function (data) {
+                                console.log(data);
+                            }, error);
+                    }, error);
+            };
+
+            var error = function () {
+                navigator.notification.alert("Unfortunately we could not add the image");
+            };
+
+            var picConfig = {
+                destinationType: Camera.DestinationType.DATA_URL,
+                targetHeight: 400,
+                targetWidth: 400
+            };
+
+            var geoConfig = {
+                maximumAge: 3000,
+                timeout: 5000,
+                enableHighAccuracy: true
+            };
+
+            var geoSuccess = function (data) {
+                location = {
+                    latitude: data.coords.latitude,
+                    longitude: data.coords.longitude
+                };
+
+                navigator.camera.getPicture(picSuccess, error, picConfig);
+            };
+
+            navigator.geolocation.getCurrentPosition(geoSuccess, error, geoConfig);
+        },
+
+        loadPhotos: loadPhoto,
+    });
+
+})();
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,106 +1,10 @@
 (function () {
 
     window.everlive = new Everlive("sz0Zz1ZApYIzWbTh");
-    var images = everlive.data("PictureInfo");
-
-    var loadPhoto = function () {
-        images.get()
-            .then(function (data) {
-                    console.log(data);
-                    var files = [];
-                    data.result.forEach(function (image) {
-                        files.push(image.Img.Uri);
-                    });
-
-                    $('#images').kendoMobileListView({
-                        dataSource: files,
-                        template: '<img src="#: data #">'
-                    });
-                },
-                function (error) {
-                    console.log(error);
-                })
-    };
-
-    //if there are any photos already in the db ... show them immediatly
-    loadPhoto();
-
-    var geolocationError = function (data) {
-        console.log('---some err---');
-        console.log(data);
-    };
-
-    var geolocationOptions = {
-        maximumAge: 3000,
-        timeout: 5000,
-        enableHighAccuracy: true
-    }
-
-    function createEverliveObject(data) {
-        console.log('FINAL DATA TO SAVE IN EVERLIVE...');
-        console.log(data.img);
-        everlive.Files.create({ //if you took the pic...create a file in everlive
-            Filename: Math.random().toString(36).substring(2, 15) + ".jpg", //give it random name
-            ContentType: "image/jpg", // config
-            base64: data //more config
-        })
-            .then(loadPhoto);
-    }
-
-    window.listView = kendo.observable({
-        addImage: function () {
-            var location = {};
-
-            var picSuccess = function (data) {
-
-                window.everlive.Files.create({ //if you took the pic...create a file in everlive
-                        Filename: Math.random().toString(36).substring(2, 15) + ".jpg", //give it random name
-                        ContentType: "image/jpg", // config
-                        base64: data //more config
-                    },
-                    function (picData) {
-                        window.everlive.data('PictureInfo').create({
-                                'Img': picData.result,
-                                'Location': location
-                            },
-                            function (data) {
-                                console.log(data);
-                            }, error);
-                    }, error);
-            };
-
-            var error = function () {
-                navigator.notification.alert("Unfortunately we could not add the image");
-            };
-
-            var picConfig = {
-                destinationType: Camera.DestinationType.DATA_URL,
-                targetHeight: 400,
-                targetWidth: 400
-            };
-
-            var geoConfig = {
-                maximumAge: 3000,
-                timeout: 5000,
-                enableHighAccuracy: true
-            };
-
-            var geoSuccess = function (data) {
-                location = {
-                    latitude: data.coords.latitude,
-                    longitude: data.coords.longitude
-                };
-
-                navigator.camera.getPicture(picSuccess, error, picConfig);
-            };
-
-            navigator.geolocation.getCurrentPosition(geoSuccess, error, geoConfig);
-        },
-
-        loadPhotos: loadPhoto,
-    });
-
+   
     document.addEventListener("deviceready", function () {
+        navigator.splashscreen.hide();
+        
         window.app = new kendo.mobile.Application(document.body, {
             skin: "flat",
             layout: "login-layout",
@@ -118,7 +22,6 @@
     document.addEventListener("online", onOnfline, false);
 
     function onOnfline() {
-        navigator.notification.alert("WE HAZ THE NET!!!");
         navigator.notification.beep(1);
     }
 
