56f53a92efdd286d704fbb8b7a893861f7f684ad
andrei-pl <andrey.pld@gmail.com>
2014-09-30 12:33:22 +0300

    Fix

--- bond-app/bond-app/scripts/places.js
+++ bond-app/bond-app/scripts/places.js
@@ -5,89 +5,97 @@
         var currentPosition = {};
 
         var loadLocation = function () {
+            setTimeout(
+                function(){
+                    function updateCoordinate(callback) {
+                        navigator.geolocation.getCurrentPosition(
+                          function (position) {
+                              var returnValue = {
+                                  latitude: position.coords.latitude,
+                                  longitude: position.coords.longitude
+                              }
+                              // and here you call the callback with whatever
+                              // data you need to return as a parameter.
+                              callback(returnValue);
+                          }
+                        ),
+                        errorMessage,
+                        geoConfig
+                    }
 
-            function updateCoordinate(callback) {
-                navigator.geolocation.getCurrentPosition(
-                  function (position) {
-                      var returnValue = {
-                          latitude: position.coords.latitude,
-                          longitude: position.coords.longitude
-                      }
-                      // and here you call the callback with whatever
-                      // data you need to return as a parameter.
-                      callback(returnValue);
-                  }
-                ),
-                errorMessage,
-                geoConfig
-            }
-
-            updateCoordinate(function (position) {
-                currentPosition = position;
-            });
-
-            function errorMessage(error) {
-                console.log('navigation error: ' + error);
-            }
-
-            var geoConfig = {
-                enableHighAccuracy: true
-            };
-
-            images.get()
-                .then(function (data) {
-                    data.result.forEach(function (image) {
-                        markers.push({
-                            "location": [image.Location.latitude, image.Location.longitude],
-                            "shape": "pinTarget",
-                            "tooltip": {
-                                "content": image.Address || ""
-                            }
-                        });
+                    updateCoordinate(function (position) {
+                        currentPosition = position;
                     });
-                    console.log(markers);
-                },
-                    function (error) {
-                        console.log(error);
-                    })
-                .then(function () {
-                    var address = $.ajax({
-                        type: 'GET',
-                        url: 'http://maps.google.com/maps/api/geocode/json?latlng=' + currentPosition.latitude + ',' + currentPosition.longitude + '&sensor=false',
-                        contentType: 'application/json'
-                    })
-                    return address;
-                },
-                function (error) {
-                    console.log(error);
-                })
-                .then(function (data) {
-                    if (!data.results[0].formatted_address) {
-                        location.reload();
+
+                    function errorMessage(error) {
+                        console.log('navigation error: ' + error);
+                    }
+
+                    var geoConfig = {
+                        enableHighAccuracy: true
                     };
 
-                    markers.push({
-                        "location": [currentPosition.latitude, currentPosition.longitude],
-                        "shape": "myStyle",
-                        "tooltip": {
-                            "content": data.results[0].formatted_address || ""
-                        }
-                    });
-                }, function (error) {
-                    console.log(error);
-                })
-                .then(function () {
-                    createMap();
-                },
-                function (error) {
-                    console.log(error);
-                });
-        };
+                    images.get()
+                        .then(function (data) {
+                            data.result.forEach(function (image) {
+                                markers.push({
+                                    "location": [image.Location.latitude, image.Location.longitude],
+                                    "shape": "pinTarget",
+                                    "tooltip": {
+                                        "content": image.Address || ""
+                                    }
+                                });
+                            });
+                            console.log(markers);
+                        },
+                            function (error) {
+                                console.log(error);
+                            })
+                        .then(function () {
+                            var address = $.ajax({
+                                type: 'GET',
+                                url: 'http://maps.google.com/maps/api/geocode/json?latlng=' + currentPosition.latitude + ',' + currentPosition.longitude + '&sensor=false',
+                                contentType: 'application/json'
+                            })
+                            return address;
+                        },
+                        function (error) {
+                            console.log(error);
+                        })
+                        .then(function (data) {
+                            var dataResult = {};
+                            dataResult = data;
 
+                            console.log(data.results[0].formatted_address);
+                            if (!data.results[0]) {
+                                location.reload();
+                            };
+
+                            console.log(data);
+
+                            markers.push({
+                                "location": [currentPosition.latitude, currentPosition.longitude],
+                                "shape": "myStyle",
+                                "tooltip": {
+                                    "content": data.results[0].formatted_address || ""
+                                }
+                            });
+                        }, function (error) {
+                            console.log(error);
+                        })
+                        .then(function () {
+                            createMap();
+                        },
+                        function (error) {
+                            console.log(error);
+                        });
+                }, 5000);
+        };
+         
         function createMap() {
             $("#map").kendoMap({
                 center: [currentPosition.latitude, currentPosition.longitude],
-                zoom: 12,
+                zoom: 14,
                 layers: [{
                     type: "tile",
                     urlTemplate: "http://#= subdomain #.tile.openstreetmap.org/#= zoom #/#= x #/#= y #.png"
